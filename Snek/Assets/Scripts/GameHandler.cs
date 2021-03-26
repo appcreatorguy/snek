dfbeadd2e7b39682220dcbd65e46e92f179be7e2
appcreatorguy <manmenonsense@gmail.com>
2021-03-26 18:12:46 +0000

    Added Basic Movement

--- Snek/Assets/Scripts/GameHandler.cs
+++ Snek/Assets/Scripts/GameHandler.cs
@@ -10,12 +10,6 @@ public class GameHandler : MonoBehaviour
     void Start()
     {
         Debug.Log("GameHandler.Start()");
-
-        int number = 0;
-        FunctionPeriodic.Create(() => {
-            CMDebug.TextPopupMouse("Ding!" + number);
-            number++;
-        }, .3f);
     }
 
     // Update is called once per frame
